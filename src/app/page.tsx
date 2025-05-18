77f6201cd7ca2da2e06bd590689abe9a35a6b465
fshojaei <fatemeeshojaeii@gmail.com>
2025-05-18 21:12:36 +0330

    fix: fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>خرید شارژ</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Link
+        href="/charge"
+        className="bg-primary hover:bg-amber-200 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition"
+      >
+        خرید شارژ
+      </Link>
+    </div>
+  );
 }
