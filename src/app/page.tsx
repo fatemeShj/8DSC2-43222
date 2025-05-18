9379f7f71cd08ac0706ec68233ba51ee11b62933
fshojaei <fatemeeshojaeii@gmail.com>
2025-05-18 21:14:55 +0330

    fix: fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen">
       <Link
         href="/charge"
-        className="bg-primary hover:bg-amber-200 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition"
+        className="bg-primary hover:bg-amber-200 text-black font-bold py-5 px-10 rounded-lg shadow-lg transition"
       >
         خرید شارژ
       </Link>
