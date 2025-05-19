67b85835b8ee65eea3f1a3236012582de5bd0c14
fshojaei <fatemeeshojaeii@gmail.com>
2025-05-19 14:30:06 +0330

    fix: fix stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         href="/charge"
         className="bg-primary hover:bg-amber-200 text-black font-bold py-5 px-10 rounded-lg shadow-lg transition"
       >
-        خرید شارژ
+        خرید آنلاین شارژ
       </Link>
     </div>
   );
